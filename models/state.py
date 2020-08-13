15bbc28846113707fef302789372d0cf5fdf3b0e
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-13 00:15:25 +0000

    [Add]: Task 6 -> DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref='state',
+                          cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(sefl):
+        """Asociete cities and states"""
+        dict_cities = models.storage.all(City)
+        _list = []
+        for city in dict_cities.values():
+            if city.states.id == sefl.id:
+                _list.append(city)
+        return _list
