4742b221252ae7bb86daa5c21829b96395f6ea41
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-14 05:24:55 +0000

    [fix]: place

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 from sqlalchemy.orm import relationship
 
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
@@ -18,7 +19,8 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, default=0, nullable=False)
         latitude = Column(Float, nullable=False)
         longitude = Column(Float, nullable=False)
-        reviews = relationship('Review', backref="place", cascade="all, delete-orphan")
+        reviews = relationship('Review', backref="place",
+                               cascade="all, delete-orphan")
     else:
         city_id = ""
         user_id = ""
@@ -27,11 +29,11 @@ class Place(BaseModel, Base):
         number_rooms = 0
         number_bathrooms = 0
         max_guest = 0
-        price_by_night = 0.0
+        price_by_night = 0
         latitude = 0.0
         longitude = 0.0
-        
-        
+        amenity_ids = []
+
     @property
     def reviews(self):
         return type(self).reviews
