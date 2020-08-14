163b2551a839336dc8fe055d7abb828c10921255
Camilo Tobon <1611@holbertonschool.com>
2020-08-13 22:53:46 -0500

    add

--- models/user.py
+++ models/user.py
@@ -10,8 +10,15 @@ from os import getenv
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="state", cascade="all, delete-orphan")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="state", cascade="all, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+        places = ""
