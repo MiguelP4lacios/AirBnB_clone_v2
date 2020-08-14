87fe8caeeb576677550ae8f2d2d05859b66debe6
Camilo Tobon <1611@holbertonschool.com>
2020-08-13 21:53:05 -0500

    adding states and places

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from sqlalchemy.orm import relationship
 
 
 
@@ -12,3 +12,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="state", cascade="all, delete-orphan")
