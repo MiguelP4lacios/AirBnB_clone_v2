cfe0082035a31681630ea4ea8b186fa77c9d0a02
Camilo Tobon <1611@holbertonschool.com>
2020-08-13 20:51:10 -0500

    adding object user to list in db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -40,7 +40,7 @@ class DBStorage:
     def all(self, cls=None):
         """all objects
         """
-        classes = ['State', 'City']
+        classes = ['State', 'City', 'User']
         dict_objs = {}
 
         if cls:
