5c996c1f2891d16a3c2fd2e697b027d339773c82
Miguel Palacios <oswaldo9605@gmail.com>
2020-08-14 05:18:48 +0000

    [fix] city

--- models/place.py
+++ models/place.py
@@ -7,18 +7,31 @@ from sqlalchemy.orm import relationship
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
-    reviews = relationship('Review', backref="place", cascade="all, delete-orphan")
-
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, default=0, nullable=False)
+        number_bathrooms = Column(Integer, default=0, nullable=False)
+        max_guest = Column(Integer, default=0, nullable=False)
+        price_by_night = Column(Integer, default=0, nullable=False)
+        latitude = Column(Float, nullable=False)
+        longitude = Column(Float, nullable=False)
+        reviews = relationship('Review', backref="place", cascade="all, delete-orphan")
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0.0
+        latitude = 0.0
+        longitude = 0.0
+        
+        
     @property
     def reviews(self):
-            return type(self).reviews
+        return type(self).reviews
