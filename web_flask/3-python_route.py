eeae4b4d00a22e1534b3267a34ab90bd39f6ea71
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-02 15:53:35 +0000

    [feature]: python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module add route
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """index function
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def f_hbnb():
+    """hbnb function
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """c function
+    """
+    return "C {}".format(text).replace("_", " ")
+
+#host:5000/python/(text)
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is_cool"):
+    """python function
+    """
+    return "Python {}".format(text).replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
