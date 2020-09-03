974df6fa46d906d339667663f3e2b1a8cf2e72f2
Miguel Palacios <oswaldo9605@gmail.com>
2020-09-03 00:05:36 +0000

    [fix]: init

--- models/base_model.py
+++ models/base_model.py
@@ -23,15 +23,27 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        else:
+            if "updated_at" in kwargs and "updated_at" in kwargs\
+                    and "__class__" in kwargs:
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+                del kwargs['__class__']
+            else:
+                self.id = str(uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -40,18 +52,18 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
+        self.updated_at=datetime.now()
         models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        my_dict = dict(self.__dict__)
+        my_dict=dict(self.__dict__)
         if '_sa_instance_state' in my_dict.keys():
             del my_dict['_sa_instance_state']
-        my_dict["__class__"] = str(type(self).__name__)
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict["__class__"]=str(type(self).__name__)
+        my_dict["created_at"]=self.created_at.isoformat()
+        my_dict["updated_at"]=self.updated_at.isoformat()
         return my_dict
 
     def delete(self):
